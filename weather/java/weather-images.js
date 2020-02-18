61b98dcfb272a6a93c9b8f1f11ef10f70f310117
AdrianTolbert <59660472+AdrianTolbert@users.noreply.github.com>
2020-02-17 20:29:34 -0700

    commit

--- weather/java/weather-images.js
+++ weather/java/weather-images.js
@@ -10,3 +10,21 @@ const loadImages = (image) => {
 imagesToLoad.forEach((img) => {
     loadImages(img);
   });
+
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((source) => {
+      observer.observe(source);
+    });
+  } else {
+    imagesToLoad.forEach((source) => {
+      loadImages(source);
+    });
+  }
