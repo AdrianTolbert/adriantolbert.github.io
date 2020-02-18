f44cbda1303f30abfba2e9103f90647779fad7da
AdrianTolbert <59660472+AdrianTolbert@users.noreply.github.com>
2020-02-18 09:12:16 -0700

    commit

--- weather/java/weather-images.js
+++ weather/java/weather-images.js
@@ -28,3 +28,38 @@ imagesToLoad.forEach((img) => {
       loadImages(source);
     });
   }
+
+
+  let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+   "June",
+   "July",
+   "August",
+   "September",
+   "October",
+   "November",
+   "December"
+
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + d.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate;
+document.getElementById("lastmodified").textContent = new Date(document.lastModified);
